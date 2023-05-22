670f796b7cbc368a1257e269e65176e9c1b6f7fc
Giovanni Bussi <giovanni.bussi@gmail.com>
2023-05-22 14:44:21 +0200

    fixed warning with recent grep

--- regtest/scripts/fixzeroes.sh
+++ regtest/scripts/fixzeroes.sh
@@ -2,9 +2,9 @@
 
 file=$1
 
-grep -q "\x00" $file  && exit 0
+grep -q '\x00' $file  && exit 0
 
-sed "s/-\(0\.0*0 \)/ \1/g; 
-     s/-\(0\.0*0$\)/ \1/g" $file > $file.$$.tmp
+sed 's/-\(0\.0*0 \)/ \1/g; 
+     s/-\(0\.0*0$\)/ \1/g' $file > $file.$$.tmp
 mv $file.$$.tmp $file
 
